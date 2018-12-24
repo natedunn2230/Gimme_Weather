57afd183ea51d2051592751a5315bc6c55da905a
natedunn2230 <dunn.113@wright.edu>
2018-12-23 20:33:11 -0500

    Initial commit.

---
+++ application/GeoUtility.py
@@ -0,0 +1,10 @@
+import geopy
+
+geolocator = geopy.Nominatim(user_agent="Weather_Twitter_Bot")
+
+def find_coordinates(address):
+    location = geolocator.geocode(address)
+    return {'latitude': location.latitude, 'longitude': location.longitude}
+
+def find_location(lat, long):
+    pass
---
+++ application/TwitterBot.py
@@ -0,0 +1,9 @@
+import requests
+import GeoUtility
+
+
+
+
+
+print(GeoUtility.find_coordinates("Troy Ohio"))
+print(GeoUtility.find_coordinates("Lima Ohio"))
