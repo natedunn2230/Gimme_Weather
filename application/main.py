6e5d78be778158dcca7e14a0337e60a3d17cb44e
NateDunn2230 <ndunn2230@gmail.com>
2019-01-06 17:57:31 -0500

    Added timestamp for weather data.

--- application/main.py
+++ application/main.py
@@ -1,11 +1,12 @@
 import WeatherUtility
 from TwitterUtility import TwitterUtility
+import time
 
 if __name__ == "__main__":
     while True:
         addr = input("Enter Address for weather data: ")
         description, temperature, humidity, wind = WeatherUtility.get_weather_data(addr)
-        tweet = "WEATHER REPORT FOR {}:\n{}\n{}°F\n{}% humidity\n{} MPH winds".format(addr.upper(), description, temperature, humidity, wind)
+        tweet = "WEATHER FOR {} (as of {})\n{}\n{}°F\n{}% humidity\n{} MPH winds".format(addr.upper(), time.strftime("%Y-%m-%d %H:%M"), description, temperature, humidity, wind)
         print(tweet)
         tweeter = TwitterUtility()
         tweeter.post_tweet(tweet)
